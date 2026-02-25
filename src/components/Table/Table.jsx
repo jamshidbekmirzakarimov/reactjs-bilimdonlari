25d7d626ef1d9a912db42bcf4302b44565848bdd
saymovadilbek001-art <saymovadilbek001@gmail.com>
2026-02-25 15:28:36 +0500

    Add Table component with styles
    Introduce a styled Table component: added src/components/Table/Table.css with table styles (full-width, bordered cells, padding, centered text, header styling) and updated src/components/Table/Table.jsx to import the stylesheet and replace the placeholder div with semantic table markup including a header and sample rows.

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
-
+import './Table.css'
 const Table = () => {
   return (
-    <div>
-      Table
-    </div>
+  <div className="tableWrapper">
+  <table className="customTable">
+    <thead>
+      <tr>
+        <th colSpan="3" className="title">Table</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Name</td>
+        <td>Phone</td>
+        <td>Age</td>
+      </tr>
+      <tr>
+        <td>Jamshidbek</td>
+        <td>123456789</td>
+        <td>25</td>
+      </tr>
+      <tr>
+        <td>Akbar</td>
+        <td>987654321</td>
+        <td>30</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
   )
 }
 
